50d005fe4105dd178a4e76ef595aa6370a9a0257
adrian badulescu <adrianb2104@gmail.com>
2026-06-26 08:57:16 +0200

    eco bon

--- src/environments/environment.capacitor.ts
+++ src/environments/environment.capacitor.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.0.17',
+  apiUrl: 'http://192.168.43.142',
   poweredBy: 'QRFE Capacitor LAN',
   printerAgentDownloadUrl:
     'https://github.com/adrian-badulescu/Printer-Agent/releases/download/v1.2.4/URSPrinterAgentSetup.exe',
